72e01105e947f2b8d391de3067cffb5f2de71e46
Andreapiani95 <andreapiani1995@gmail.com>
2023-06-09 15:25:03 +0200

    Esercizio Completato

---
+++ js/script.js
@@ -0,0 +1,4 @@
+const nameFromPrompt = prompt ('Inserisci il tuo nome')
+const lastNameFromPrompt = prompt ('Inserisci il tuo cognome')
+const colorFromPrompt = prompt ('Inserisci il tuo colore preferito')
+document.getElementById('password').innerHTML = nameFromPrompt + lastNameFromPrompt + colorFromPrompt + '101'
